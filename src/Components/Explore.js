383e3afc942e0cf70691d4a50668415ec53cd9d7
Pinchon Léo <leopinchonspa@gmail.com>
2022-02-11 18:55:31 +0100

    création des dossiers components + screen + début de la page home (btn explore)

---
+++ src/Components/Explore.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/Explore.css";
+export default function Explore() {
+  return (
+   
+      <a  href="#" className="btn-explore">Explore</a>
+    
+  );
+}
---
+++ src/screen/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Explore from './../Components/Explore';
+
+export default function Home() {
+  return (
+    <div className='home'>
+    <h1>Space</h1>
+    <Explore/>
+    </div>
+  )
+}
